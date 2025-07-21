c3416a0803a284cdb1cfaf66b1bfcb500dfe9020
shresthveoji <satwikj746@gmail.com>
2025-07-21 06:36:11 +0000

    new file

---
+++ js_basics/05_memory.js
@@ -0,0 +1,10 @@
+// stack and heap - memory 
+// let myname = "satwikjaiswal"
+
+let anothername = myname
+ anothername = "chaiorcode"
+ let anothername = myname
+
+console.log(myname);
+
+console.log(anothername)
