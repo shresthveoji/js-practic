bdbb3beded8e3cb57dd15761bc4fe7ef2dcffdcc
shresthveoji <satwikj746@gmail.com>
2025-07-25 11:30:07 +0000

    file edit

--- js_basics/06_strings.js
+++ js_basics/06_strings.js
@@ -3,9 +3,24 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Data");
 
-console.log(`Hello my name is ${name} whats yours? `);
+/* console.log(`Hello my name is ${name} whats yours? `);
 
-console.log(`My Repo Count is ${repoCount}`);
+console.log(`My Repo Count is ${repoCount}`); */
 
-const gamename = new strings('rameshhvc')
+const gamename = new String('   rameshhvc   ')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+const newString = gamename.substring(0,6)
+console.log(newString);
+
+const anotherString = gamename.slice(-8,4)
+console.log(anotherString);
+
+const spacename = gamename.trim()
+console.log(spacename);
+
+console.log(gamename.trim());
+console.log(gamename.trimEnd());
 
