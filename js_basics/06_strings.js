16ddff4bb24ca1f20217f451da6654dcfc47c2b6
shresthveoji <satwikj746@gmail.com>
2025-07-25 07:26:25 +0000

    edit

---
+++ js_basics/06_strings.js
@@ -0,0 +1,11 @@
+const name = "Ramesh"
+const repoCount = 50 
+
+// console.log(name + repoCount + " Data");
+
+console.log(`Hello my name is ${name} whats yours? `);
+
+console.log(`My Repo Count is ${repoCount}`);
+
+const gamename = new strings('rameshhvc')
+
